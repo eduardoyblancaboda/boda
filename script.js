d63ad996bac9e70a3d731fab3249b51635a151ee
eduardoyblancaboda <eduardoyblancaboda@gmail.com>
2026-02-20 00:29:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function togglePlay() {
     const audio = document.getElementById("background-music");
     const btn = document.getElementById("audioBtn");
+    
     if (audio.paused) {
-        audio.play().catch(() => {});
+        audio.play().catch((error) => {
+            console.log("La reproducción automática fue bloqueada por el navegador.");
+        });
         btn.innerHTML = "|| PAUSAR MÚSICA";
     } else {
         audio.pause();
